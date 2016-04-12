ab542dd15b3ff71409a3f8811cfbef3992fb68fa
eugenp <hanriseldon@gmail.com>
2016-04-12 23:04:06 +0300

    initial commit of the new hypermedia project

---
+++ src/main/java/com/baeldung/SpringHypermediaApiApplication.java
@@ -0,0 +1,12 @@
+package com.baeldung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHypermediaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHypermediaApiApplication.class, args);
+	}
+}
