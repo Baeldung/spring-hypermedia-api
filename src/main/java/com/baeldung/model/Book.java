9fa57bf48e5d078b799369b1def8de24c2066b2b
Christian Rädel <christian.raedel@gmail.com>
2016-09-06 02:55:12 +0200

    BAEL-11: Added @GeneratedValue annotation to Book model.

--- src/main/java/com/baeldung/model/Book.java
+++ src/main/java/com/baeldung/model/Book.java
@@ -2,6 +2,7 @@ package com.baeldung.model;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
@@ -12,6 +13,7 @@ public class Book {
 
     @JsonIgnore
     @Id
+    @GeneratedValue
     private long id;
 
     @Column(nullable = false)
